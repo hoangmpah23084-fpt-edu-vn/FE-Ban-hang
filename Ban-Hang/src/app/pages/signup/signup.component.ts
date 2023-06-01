2da3c42de0baad685c6ea1cfbc6c0358178cbbab
doantheminh <doantheminh13@gmail.com>
2023-06-01 20:11:46 +0700

    feat:FA-24 FA-24:Signin refs:FA-24

--- Ban-Hang/src/app/pages/signup/signup.component.ts
+++ Ban-Hang/src/app/pages/signup/signup.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import {FormBuilder,Validators,FormGroup} from "@angular/forms"
+import { FormBuilder, Validators, FormGroup } from "@angular/forms"
 import { AuthService } from 'src/app/service/auth.service';
 import { Router } from '@angular/router';
-import { Usse } from 'src/app/interface/auth';
+import { User } from 'src/app/interface/auth';
 
 @Component({
   selector: 'app-signup',
@@ -10,33 +10,32 @@ import { Usse } from 'src/app/interface/auth';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
-  user!:Usse
-  submitted:boolean= false
-   formSignup = this.formBuilder.group({
-      name:['',[Validators.required]],
-      email:['',[Validators.required, Validators.email]],
-      password:['',[Validators.required, Validators.minLength(8)]],
-      confirmPassword: ['', [Validators.required]]
-   },{Validators:this.checkPassword})
+  user!: User
+  submitted: boolean = false
+  formSignup = this.formBuilder.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    confirmPassword: ['', [Validators.required]]
+  }, { Validators: this.checkPassword })
 
-   constructor(private formBuilder: FormBuilder,
+  constructor(private formBuilder: FormBuilder,
     private auth: AuthService,
-    private router:Router){}
+    private router: Router) { }
 
-   checkPassword(form: FormGroup){
+  checkPassword(form: FormGroup) {
     const password = form.get('password')?.value;
     const confirmPassword = form.get('confirmPassword')?.value;
-    if(password === confirmPassword)
+    if (password === confirmPassword)
       return null;
+    return { notMath: true };
+  }
 
-    return {notMath: true};
-   }
-
-   onHandleSubmit(){
+  onHandleSubmit() {
     this.submitted = true;
-    this.auth.signup(this.formSignup.value).subscribe((data)=>{
+    this.auth.signup(this.formSignup.value).subscribe((data) => {
       console.log(data);
       this.router.navigate(['/signin']);
     })
-   }
+  }
 }
--- Ban-Hang/src/app/service/auth.service.ts
+++ Ban-Hang/src/app/service/auth.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http"
+import { HttpClient } from "@angular/common/http"
 import { Observable } from 'rxjs';
-import { Usse } from '../interface/auth';
+import { Signin, User } from '../interface/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   API_URL = 'http://localhost:8080/api'
-  constructor(private http:HttpClient) { }
-  
-  signup(user: Usse ): Observable<Usse>{
-    return this.http.post<Usse>(`${this.API_URL}/signup`,user)
+  constructor(private http: HttpClient) { }
+
+  signup(user: User): Observable<User> {
+    return this.http.post<User>(`${this.API_URL}/signup`, user)
+  }
+
+  signin(user: User): Observable<User> {
+    return this.http.post<User>(`${this.API_URL}/signin`, user)
   }
 }
