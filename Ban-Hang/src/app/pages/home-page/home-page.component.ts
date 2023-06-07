50de9f3b1e1f6617608dcb10809317b31420595f
doantheminh <doantheminh13@gmail.com>
2023-06-07 23:00:39 +0700

    feat:FA-83 Display the list to the home page refs:FA-83

--- Ban-Hang/src/app/pages/home-page/home-page.component.ts
+++ Ban-Hang/src/app/pages/home-page/home-page.component.ts
@@ -3,6 +3,8 @@ import { IProduct } from 'src/app/interface/product';
 import { AddToCartService } from 'src/app/service/add-to-cart.service';
 import { ProductService } from 'src/app/service/product.service';
 import Swal from 'sweetalert2';
+import { CategoryService } from '../../service/category.service';
+import { ICategory } from 'src/app/interface/category';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
@@ -11,15 +13,23 @@ import Swal from 'sweetalert2';
 export class HomePageComponent {
   products!: IProduct[]
   carts: any = this.cartService.getToCart()
+  category!: ICategory[]
+  allCategory!: ICategory[];
+
 
   constructor(private productService: ProductService,
-    private cartService: AddToCartService) {
+    private cartService: AddToCartService, private cate: CategoryService) {
     this.productService.getProducts().subscribe((response: any) => {
       this.products = response.data
       console.log(this.carts);
 
     }
     )
+    this.cate.getCategoryAll().subscribe((response: any) => {
+      console.log(response.data)
+      this.category = response.data
+      this.allCategory = response.data;
+    })
 
   }
 
@@ -52,7 +62,7 @@ export class HomePageComponent {
       showConfirmButton: false,
       timer: 1500
     })
-  } 
+  }
 
   formatCurrency(value: number): string {
     const formatter = new Intl.NumberFormat('vi-VN', {
